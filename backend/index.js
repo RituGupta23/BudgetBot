ce233230d8c9f995319858ce543ebc977883936e
RituGupta23 <ritugupta232004@gmail.com>
2025-06-24 21:41:20 +0530

    Routes in index

--- backend/index.js
+++ backend/index.js
@@ -2,12 +2,20 @@ const express = require('express');
 const dotenv = require('dotenv');
 const cors = require('cors');
 const connectDB = require('./config/db');
+const expenseRoutes = require('./routes/expenseRoutes');
+const whatsappRoutes = require('./routes/whatsappRoutes');
+const authRoutes = require('./routes/authRoutes');
 
 dotenv.config();
 const app = express();
 
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cors());
+
+app.use('/api/auth', authRoutes);
+app.use('/api/expenses', expenseRoutes);
+app.use('/api/whatsapp', whatsappRoutes);
 
 app.get('/health', (req, res) => {
   res.send('BudgetBot Health Check: Server is running');
