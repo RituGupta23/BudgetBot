f95afa01d0b35a21cfd2193894f8e5a80b2ded0d
Ritu Gupta <133960872+RituGupta23@users.noreply.github.com>
2025-06-28 16:13:36 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -11,7 +11,10 @@ const app = express();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+  origin: process.env.CLIENT_URL,
+  credentials: true
+}));
 
 app.use('/api/auth', authRoutes);
 app.use('/api/expenses', expenseRoutes);
